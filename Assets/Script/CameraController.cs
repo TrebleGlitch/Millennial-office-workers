40508120c5c9645acc218a02e4122ff2e27d1f5d
Victormaa <389796272@qq.com>
2026-06-14 16:49:35 +0800

    fix camera

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -3,16 +3,16 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    public CinemachineVirtualCamera[] cameras; // 0: Left, 1: Center, 2: Right
-    private int currentIndex = 0; // 初始在中间
+    public CinemachineVirtualCamera[] cameras; // 0: Left, 1: Center, 2: Right ; 3 back
+    private int currentIndex = 1; // 初始在中间
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && currentIndex > 0)
+        if (Input.GetKeyDown(KeyCode.A))
         {
             SwitchCamera(currentIndex - 1);
         }
-        else if (Input.GetKeyDown(KeyCode.D) && currentIndex < cameras.Length - 1)
+        else if (Input.GetKeyDown(KeyCode.D))
         {
             SwitchCamera(currentIndex + 1);
         }
@@ -22,7 +22,14 @@ public class CameraController : MonoBehaviour
     {
         // 降低当前相机的优先级
         cameras[currentIndex].Priority = 10;
-        currentIndex = newIndex;
+
+        if(newIndex < 0)
+            currentIndex = cameras.Length - 1;
+        else if(newIndex >= cameras.Length)
+            currentIndex = 0;
+        else
+            currentIndex = newIndex;
+
         // 提高新相机的优先级，CinemachineBrain会自动进行平滑过渡
         cameras[currentIndex].Priority = 11;
     }
